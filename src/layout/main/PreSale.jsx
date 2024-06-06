97e713b11690f1850fc185508fb6c7d0c8f00901
rashidpkn <muhammed916rashid@gmail.com>
2024-06-06 14:18:38 +0530

    font

--- src/layout/main/PreSale.jsx
+++ src/layout/main/PreSale.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function PreSale()  {
   return (
-    <div className="scrolling-strip home-hero-marquee text-lg font-bold py-2 w-full bg-black/90">
+    <div className="scrolling-strip home-hero-marquee text-xs font-medium py-2 w-full bg-black/90">
                 
     <div className="scroll-animation">    
       <span className="strip-item">PRE-SALE LIVE</span>
--- src/pages/home/Hero.jsx
+++ src/pages/home/Hero.jsx
@@ -7,44 +7,44 @@ export default function Hero()  {
     const {isConnected,address} = useAccount()
   return (
     <section className='home-hero min-h-screen bg-cover bg-center text-white relative'>
-            <div className="container mx-auto px-[] lg:px-[1%]  flex flex-col lg:flex-row gap-24 lg:gap-0  justify-between pt-28">
+            <div className="container mx-auto  lg:px-[5%]  flex flex-col lg:flex-row gap-24 lg:gap-0  justify-between pt-24">
                         <div className="text-center lg:text-left">
-                            <p className='font-conthrax font-semibold text-lg 2xl:text-xl'>WELCOME TO</p>
-                            <h1 className='font-slant text-6xl 2xl:text-8xl'>DIGIHORSE EMPIRES</h1>
-                            <p className='font-conthrax font-semibold text-lg 2xl:text-xl'>RIDING THE WAVE OF THE WEB3 GAMING REVOLUTION</p>
+                            <p className='font-conthrax text-sm 2xl:text-xl'>WELCOME TO</p>
+                            <h1 className='font-slant text-7xl 2xl:text-8xl'>DIGIHORSE EMPIRES</h1>
+                            <p className='font-conthrax text-sm 2xl:text-xl'>RIDING THE WAVE OF THE WEB3 GAMING REVOLUTION</p>
                         </div>
-                        <div className="max-w-[400px] w-full home-hero-widget p-5 space-y-5">
-                            <p className='text-center font-vcr'>Buy now before Prices increase!</p>
+                        <div className="max-w-[400px] w-full home-hero-widget p-5 space-y-3">
+                            <p className='text-center font-vcr text-sm'>Buy now before Prices increase!</p>
                                     
-                                    <div className="grid grid-cols-2 gap-5">
-                                        <div className="w-full home-hero-widget-stage1 p-5 space-y-2">
+                                    <div className="grid grid-cols-2 gap-3">
+                                        <div className="w-full home-hero-widget-stage1 p-5 space-y-1">
                                             <div className="flex justify-between">
-                                                <p className='text-xs font-conthrax'>STAGE 1</p>
+                                                <p className='text-[10px] font-conthrax'>STAGE 1</p>
                                                 <p className='text-sm font-vcr'><span></span> Live</p>
                                             </div>
-                                            <p className='text-center font-vcr text-3xl'>$0.0125</p>
+                                            <p className='text-center font-vcr text-xl'>$0.0125</p>
                                         </div>
-                                        <div className="w-full home-hero-widget-stage2 p-5 space-y-2">
+                                        <div className="w-full home-hero-widget-stage2 p-5 space-y-1">
                                         <div className="flex justify-between">
-                                                <p className='text-xs font-conthrax'>STAGE 2</p>
+                                                <p className='text-[10px] font-conthrax'>STAGE 2</p>
                                                 <p className='text-sm font-vcr'><span></span> 66.67%</p>
                                             </div>
-                                            <p className='text-center font-vcr text-3xl'>$0.0075</p>
+                                            <p className='text-center font-vcr text-xl'>$0.0075</p>
                                         </div>
                                         <div className="w-full col-span-2 home-hero-widget-stage2 p-5 flex justify-between items-center">
                                                     <div className="">
-                                                    <p className='text-xs font-conthrax'>LISTING PRICE</p>
-                                                    <p className='text-center font-vcr text-3xl'>$0.0075</p>
+                                                    <p className='text-[10px] font-conthrax'>LISTING PRICE</p>
+                                                    <p className='text-center font-vcr text-xl'>$0.0075</p>
                                                     </div>
                                                     <p className='text-sm font-vcr'>566.67%</p>
                                         </div>
 
 
                                     </div>
-                                    <div className="space-y-2">
+                                    <div className="space-y-1">
 
-                                    <div className="flex justify-between items-center px-5">
-                                            <p className='font-conthrax text-xs'>PRE-SALE STAGE 1</p>
+                                    <div className="flex justify-between items-center px-2 text-[10px]">
+                                            <p className='font-conthrax '>PRE-SALE STAGE 1</p>
                                             <p className='font-vcr flex items-center gap-1'>
                                               <img src="/images/home/logo.webp" width={26} height={26} alt="" />
                                                1 DIGI = $0.0075
@@ -54,18 +54,18 @@ export default function Hero()  {
                                         <div className="home-hero-widget-progressbar w-[40%] h-full"></div>
                                     </div>
 
-                                    <div className="flex justify-between px-5 font-vcr text-xl">
+                                    <div className="flex justify-between  font-vcr px-2 text-[12px]">
                                             <p className=''>$212,175.00</p>
                                             <p>$450,000</p>
                                     </div>
                                     </div>
 
-                            <button className='home-hero-widget-connect-wallet w-full py-4' onClick={()=>open()}>
+                            <button className='home-hero-widget-connect-wallet w-full py-4 ' onClick={()=>open()}>
                             {isConnected ? address?.substring(0,5) + '...' + address?.substring(address.length-5,address.length) :  'CONNECT WALLET'}
                                 </button>
 
 
-                            <p className='text-center font-vcr'>Don't have an account? <span className='text-[#00FF4E]'>Register</span></p>
+                            <p className='text-center font-vcr text-sm'>Don't have an account? <span className='text-[#00FF4E]'>Register</span></p>
 
                         </div>
             </div>
