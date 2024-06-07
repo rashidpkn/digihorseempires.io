6e7e31fa9618ce4229dbd3321cd13622301b3051
rashidpkn <muhammed916rashid@gmail.com>
2024-06-07 13:52:05 +0530

    table

--- src/pages/home/Leaderboard.jsx
+++ src/pages/home/Leaderboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Leaderboard() {
   return (
     <section className='w-full home-leaderboard-bg bg-no-repeat bg-cover text-white'>        
-            <div className="container mx-auto px-[1%] min-h-screen  h-full flex justify-center items-center">
+            <div className="w-[80%] mx-auto px-[1%] min-h-screen  h-full flex justify-center items-center">
                     <div className="home-leaderboard-table  bg-cover bg-no-repeat p-10 space-y-5 w-full ">
 
                         <h2 className='font-slant text-[54px] '>Top EARNERS</h2>
@@ -21,9 +21,13 @@ export default function Leaderboard() {
                                 {[1,2,3,4,5,6,7,8,9,10].map(e=>{
                                     return(
                                         <tr key={e} className='h-14'>
-                                    <td width={200}>{e}</td>
-                                    <td className='min-w-[600px]'>0XEWRTEYHD4565RETB945JB9R5K4GB945GK495EKRT909</td>
-                                    <td className='text-[30px]'>265</td>
+                                    <td width={100} className='flex gap-5 items-center '>
+                                        <img src="/images/home/red-logo.webp"  width={40} alt="" />
+                                        <p className='text-sm'>{e}</p>
+                                         
+                                         </td>
+                                    <td className='min-w-[600px] text-[16px]'>0XEWRTEYHD4565RETB945JB9R5K4GB945GK495EKRT909</td>
+                                    <td className='text-[24px]'>265</td>
                                 </tr>
 
                                     )
